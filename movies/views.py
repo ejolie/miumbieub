fe26b66caefd4a528b6b00a74a176f14a2e4d1b7
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-05-14 17:23:43 +0900

    Finish API
    Add serializers
    Add api_view actions

--- movies/views.py
+++ movies/views.py
@@ -1,29 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Genre, Movie, Rating
+from .serializers import GenreSerializer, MovieSerializer, RatingSerializer
 
 
 def index(request):
     return render(request, 'movies/app.html')
 
 
+@api_view(["GET"])
 def genres_list(request):
-    pass
+    genres = Genre.objects.all()
+    serializer = GenreSerializer(genres, many=True)
+    return Response(serializer.data)
 
 
+@api_view(["GET"])
 def genres_detail(request, pk):
-    pass
+    genre = get_object_or_404(Genre, pk=pk)
+    serializer = GenreSerializer(genre)
+    return Response(serializer.data)
 
 
+@api_view(["GET", "POST"])
 def movies_list(request):
-    pass
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+@api_view(["GET", "DELETE", "PUT"])
 def movies_detail(request, pk):
-    pass
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == 'GET':
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    elif request.method == 'DELETE':
+        movie.delete()
+        return Response({"message": "삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
 
 
-def ratings_list(request, movie_pk):
-    pass
+@api_view(["GET", "POST"])
+def ratings_list(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == "GET":
+        ratings = movie.ratings.all()
+        serializer = RatingSerializer(ratings, Many=True)
+        return Response(serializer.data)
+    else:
+        serializer = RatingSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-def ratings_detail(request, movie_pk, rating_pk):
-    pass
+@api_view(["GET", "DELETE", "PUT"])
+def ratings_detail(request, pk):
+    rating = get_object_or_404(Rating, pk=pk)
+    if request.method == 'GET':
+        serializer = RatingSerializer(rating)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = RatingSerializer(rating, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    elif request.method == 'DELETE':
+        rating.delete()
+        return Response({"message": "삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
