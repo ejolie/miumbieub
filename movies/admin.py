9671df499762945ce13eb3951d136cf59f29c27d
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-05-14 17:27:09 +0900

    Update admin site
    register models

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Genre, Movie, Rating
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Rating)
