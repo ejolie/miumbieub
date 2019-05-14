42e9ec7b429e67d7948e061b730963d20c2136f6
Jiwook Seo <jiwookseo.dev@gmail.com>
2019-05-14 16:29:38 +0900

    Add accounts feature
    Add CustomCreationForm
    Add login logout signup actions

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-app_name = "accounts"
 
+app_name = "accounts"
 urlpatterns = [
     path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
     path('signup/', views.signup, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
-from django.contrib.auth import login as auth_login, logout as logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserCustomCreationForm
 
 
 def login(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'movies:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/forms.html', {'form': form})
 
 
+@login_required
 def logout(request):
-    pass
+    auth_logout(request)
+    return redirect('movies:index')
 
 
 def signup(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+    if request.method == 'POST':
+        user_form = UserCustomCreationForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            auth_login(request, user)
+            return redirect('movies:index')
+    else:
+        user_form = UserCustomCreationForm()
+    context = {'form': user_form}
+    return render(request, 'accounts/forms.html', context)
