7b5b9d3538f2c74e22242ea808f90d2806992fa2
Eunjeong Park <wandukong@snu.ac.kr>
2019-05-15 07:10:42 +0000

    Add django.contrib.humanize in installed_apps

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'bulma',
+    'django_extensions',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,8 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'movies',
     'accounts',
-    'rest_framework',
-    'bulma'
 ]
 
 MIDDLEWARE = [
