5bf5ec528fcea09bc3593553817f22173ba8741d
Eunjeong Park <wandukong@snu.ac.kr>
2019-05-15 05:37:44 +0000

    Create __str__ method

--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth import get_user_model
 
 class Genre(models.Model):
     type = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.type
 
 
 class Movie(models.Model):
@@ -12,9 +15,14 @@ class Movie(models.Model):
     director = models.CharField(max_length=45)
     summary = models.TextField()
 
+    def __str__(self):
+        return self.genre
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='ratings')
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='ratings')
     comment = models.TextField()
     score = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.movie} | {self.score} | {self.comment}'
